a37830b9e682acf330b706622ce41b4baa68669b
SimonSkriver <simon.skriver@hotmail.com>
2026-04-14 23:29:13 +0200

    Square movement

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class CubeMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float rollSpeed = 3f;
+    private bool isMoving;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving) return;
+
+        var keyboard = Keyboard.current;
+
+        if (keyboard.wKey.isPressed) Assemble(Vector3.forward);
+        if (keyboard.sKey.isPressed) Assemble(Vector3.back);
+        if (keyboard.aKey.isPressed) Assemble(Vector3.left);
+        if (keyboard.dKey.isPressed) Assemble(Vector3.right);
+
+        void Assemble(Vector3 dir)
+        {
+            var pivotPoint = transform.position + (Vector3.down + dir) * 0.5f;
+            var axis = Vector3.Cross(Vector3.up, dir);
+            StartCoroutine(Roll(pivotPoint, axis));
+        }
+    }
+
+    IEnumerator Roll(Vector3 pivotPoint, Vector3 axis)
+    {
+        isMoving = true;
+
+        for (int i = 0; i < (90 / rollSpeed); i++)
+        {
+            transform.RotateAround(pivotPoint, axis, rollSpeed);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        isMoving = false;
     }
 }
